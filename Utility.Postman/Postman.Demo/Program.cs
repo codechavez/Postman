bfa157a8c5109b94d569855e1f51e32d554bf477
Victor A Chavez <v.a.ch.m@hotmail.com>
2017-07-13 11:45:57 -0700

    Added short demo

---
+++ Utility.Postman/Postman.Demo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Postman;
+
+namespace Postman.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Postman.EmailFrom("vac@microsoft.com")
+                .To("sample@email.com", "sample1@email.com")
+                .CC("cc@sample.com")
+                .WithSubject("Testing postman on facade")
+                .WithBody("<h1>HELLO MUNDO!</h1>")
+                .Attachments("C:\\somefile.txt")
+                .Deliver();
+        }
+    }
+}
